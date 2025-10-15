5cffc40320ad5d549fba298f85fea631dbc25f2f
Brad Gessler <bradgessler@gmail.com>
2025-10-15 16:58:56 -0700

    Initial commit

---
+++ lib/phlex/rails/template.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require_relative "template/version"
+
+module Phlex
+  module Rails
+    module Template
+      class Error < StandardError; end
+      # Your code goes here...
+    end
+  end
+end
