15868ad5c7aca519580e905f4211c2e89c7c1104
Brad Gessler <bradgessler@gmail.com>
2025-10-15 17:10:18 -0700

    Rails template handler for Phlex classes

---
+++ lib/phlex/rails/template/engine.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+module Phlex
+  module Rails
+    module Template
+      class Engine < ::Rails::Engine
+        initializer "phlex.rails.template.register_handler" do
+          ActiveSupport.on_load(:action_view) do
+            ActionView::Template.register_template_handler :rb, Phlex::Rails::Template::Handler
+          end
+        end
+      end
+    end
+  end
+end
